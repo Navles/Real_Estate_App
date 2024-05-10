2f8776ef8b0fc93d6260611d90002db412e5b978
Tamilselvan Ganesan <limatganesan@gmail.com>
2024-05-10 10:21:54 +0530

    Improving Css

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import Header from "../common/header/Header"
-import { BrowserRouter as Router,Route,Routes } from "react-router-dom"
-import Home from "../home/Home"
-import Footer from "../common/footer/Footer"
-import About from "../about/About"
-import Pricing from "../pricing/Pricing"
-import Blog from "../blog/Blog"
-import Services from "../services/Services"
-import Contact from "../contact/Contact"
+import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+import Footer from "../common/footer/Footer";
+import About from "../about/About";
+import Pricing from "../pricing/Pricing";
+import Blog from "../blog/Blog";
+import Services from "../services/Services";
+import Contact from "../contact/Contact";
 
 const Pages = () => {
   return (
@@ -15,17 +15,17 @@ const Pages = () => {
       <Router>
         <Header />
         <Routes>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/pricing' component={Pricing} />
-          <Route exact path='/contact' component={Contact} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
